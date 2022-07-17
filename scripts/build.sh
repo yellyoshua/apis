e80a771c93020d56c44a9ce50709a4929a3cb2c9
yellyoshua <usuario1529yoshua@hotmail.com>
2022-07-17 16:31:27 -0500

    change esbuild configs

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,10 @@ ESBUILD_BIN=./node_modules/.bin/esbuild
 
 ${ESBUILD_BIN} \
 	src/index.ts \
+  --platform=browser \
+  --target=es2020 \
+  --format=esm \
+  --conditions=worker,browser \
 	--bundle \
 	--minify \
 	--outfile=dist/worker.js \
