5695e4973529f9b12c59b8b456058fc85996cd95
yellyoshua <usuario1529yoshua@hotmail.com>
2022-07-24 12:54:49 -0500

    handle periodic-table to new router

--- services/periodic-table/src/index.ts
+++ services/periodic-table/src/index.ts
@@ -1,22 +1,16 @@
-import { Router, createResponse } from "../../shared";
+import { routesSetup, NewHandler } from "../../shared";
 import periodicTable from "./data/periodic-table.json";
 import newPeriodicTable from "./data/periodic-table-new.json";
 
-const handlerPeriodicTable = (_: any, headers: any) => {
-  const body = JSON.stringify(periodicTable);
-  return createResponse({ body, response: { headers } });
+const handlerPeriodicTable: NewHandler = (_: any, response) => {
+  return response.json(periodicTable, 200);
 };
 
-const handlerNewPeriodicTable = (_: any, headers: any) => {
-  const body = JSON.stringify(newPeriodicTable);
-  return createResponse({ body, response: { headers } });
+const handlerNewPeriodicTable: NewHandler = (_, response) => {
+  return response.json(newPeriodicTable, 200);
 };
 
-addEventListener("fetch", (event) => {
-  const router = new Router(event);
-
-  router.GET("/previous", handlerPeriodicTable);
-  router.GET("/latest", handlerNewPeriodicTable);
-
-  router.serve();
-});
+addEventListener("fetch", routesSetup([
+  { path: "/previous", method: 'get', handler: handlerPeriodicTable },
+  { path: "/latest", method: 'get', handler: handlerNewPeriodicTable },
+]));
